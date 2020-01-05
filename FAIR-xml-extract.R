a3c9c2a5ac43d88694da20eb0bfa1b48b771cee5
Matt Jones <gitcode@magisa.org>
2020-01-05 01:41:28 -0800

    Use mclapply to parallel process all files.
    Current state demonstrates the approach, and is used for timing performance.

--- FAIR-xml-extract.R
+++ FAIR-xml-extract.R
@@ -1,6 +1,8 @@
 library(xml2)
 library(dplyr)
 library(purrr)
+library(parallel)
+library(ggplot2)
 
 extract_value <- function(nodeset, xpath) {
   node <- xml_find_all(nodeset, xpath)
@@ -9,7 +11,10 @@ extract_value <- function(nodeset, xpath) {
 }
 
 extract_run <- function(filename) {
-  doc <- read_xml(file(filename))
+  if (is.na(filename)) {
+    return(data.frame())
+  }
+  doc <- read_xml(file(paste0("reports/", filename)))
   run_id <- extract_value(doc, "/ns0:run/id")
   obj_id <- extract_value(doc, "/ns0:run/objectIdentifier")
   suite_id <- extract_value(doc, "/ns0:run/suiteId")
@@ -21,26 +26,103 @@ extract_run <- function(filename) {
   obsoletes <- extract_value(doc, "/ns0:run/sysmeta/obsoletes")
   series_id <- extract_value(doc, "/ns0:run/sysmeta/seriesId")
 
-  runs_df <- data.frame(run_id, obj_id, series_id, suite_id, run_status, origin_mn, date_uplaoded, format_id, obsoletes, stringsAsFactors = FALSE)
+  runs_df <- data.frame(run_id, obj_id, series_id, suite_id, run_status, origin_mn, date_uplaoded, format_id, obsoletes, filename, stringsAsFactors = FALSE)
   return(runs_df)
 }
 
 extract_checks <- function(filename) {
-  doc <- read_xml(file(filename))
+  if (is.na(filename)) {
+    return(data.frame())
+  }
+  doc <- read_xml(file(paste0("reports/", filename)))
   run_id <- extract_value(doc, "/ns0:run/id")
   results <- xml_find_all(doc, "/ns0:run/result")
-  check_id <- xml_text(xml_find_all(results, "./check/id"))
-  check_name <- xml_text(xml_find_all(results, "./check/name"))
-  check_type <- xml_text(xml_find_all(results, "./check/type"))
-  check_level <- xml_text(xml_find_all(results, "./check/level"))
-  check_status <- xml_text(xml_find_all(results, "./status"))
-  check_output <- xml_text(xml_find_all(results, "./output"))
-
-  check_df <- data.frame(check_id, check_name, check_type, check_level, check_status, check_output, stringsAsFactors = FALSE) %>%
+  check_df <- map_df(.x = results, .f = extract_result) %>%
     mutate(run_id = run_id)
+
   return(check_df)
 }
 
-file_list <- list.files(path = ".", pattern = "\\.xml")
-runs <- map_df(.x = file_list, .f = extract_run)
-checks <- map_df(.x = file_list, .f = extract_checks)
+extract_result <- function(result) {
+  check_id <- xml_text(xml_find_all(result, "check/id"))
+  check_name <- xml_text(xml_find_all(result, "check/name"))
+  check_type <- xml_text(xml_find_all(result, "check/type"))
+  check_level <- xml_text(xml_find_all(result, "check/level"))
+  check_status <- xml_text(xml_find_all(result, "status"))
+  # Output nodes can be repeated, so concatenate them together into one string
+  output_nodes <- xml_find_all(result, "output")
+  check_output <- paste(xml_text(output_nodes), sep = ' ', collapse = '')
+  result_df <- data.frame(check_id, check_name, check_type, check_level, check_status, check_output, stringsAsFactors = FALSE)
+  return(result_df)
+}
+
+#' Split up a vector into equal sized chunks
+#'
+#' @param input An input vector.
+#' @param by The length of the resulting vectors.
+#' @examples
+#' vec <- 1:13
+#' slice(vec, 3)
+slice <- function(input, by=2) {
+  starts <- seq(1,length(input),by)
+  tt <- lapply(starts, function(y) input[y:(y+(by-1))])
+  lmap(tt, function(x) x[!is.na(x)])
+}
+
+prof <- function(file_list, sizes) {
+  times <- c()
+  for (size in sizes) {
+    print(paste0("Processing size: ", size))
+    current_list <- sample(file_list, size=size, replace=F)
+    runs_time <- system.time(runs <- map_df(.x = current_list, .f = extract_run))
+    checks_time <- system.time(checks <- map_df(.x = current_list, .f = extract_checks))
+    elapsed <- runs_time[[3]] + checks_time[[3]]
+    times <- append(times, elapsed)
+    print(paste0("Size / Elapsed: ", size, " / ", elapsed))
+  }
+  return(times)
+}
+sizes_1 <- c(10, 25, 50, 100, 200, 300, 400, 500)
+runtimes_1 <- prof(file_list_full, sizes)
+
+process_reports <- function(file_chunk) {
+  runs <- map_df(.x = file_chunk, .f = extract_run)
+  checks <- map_df(.x = file_chunk, .f = extract_checks)
+  return(list(runs,checks))
+}
+
+
+par_process <- function() {
+  #file_list_full <- list.files(path = "reports", pattern = "\\.xml")
+  file_list <- sample(file_list_full, size=100, replace=F)
+  file_chunks <- slice(file_list, 5)
+
+  # Time it serially for a benchmark
+  # system.time({
+  #   output_serial <- process_reports(file_list)
+  # })
+
+  # Time it in parallel and compare results
+  timedata = data.frame()
+  for (numCores in (c(20))) {
+    print(paste0("Timing cores: ", numCores))
+    t <- system.time({
+      output_parallel <- mclapply(file_chunks, process_reports, mc.cores = numCores)
+      runs <- bind_rows(map(output_parallel, 1))
+      checks <- bind_rows(map(output_parallel, 2))
+    })
+    timings <- list(numCores=numCores, user=t[[1]]+t[[4]], system=t[[2]]+t[[5]], elapsed=t[[3]])
+    print(as.data.frame(timings))
+    timedata <- bind_rows(timedata, timings)
+  }
+}
+
+par_process()
+
+# ggplot(timedata_all, mapping = aes(x = numCores, y = elapsed)) +
+#   geom_point() +
+#   geom_line() +
+#   facet_wrap(facets = ~chunksize, ncol = 5) +
+#   theme_bw()
+#geom_point(mapping = aes(y=user+system))
+
