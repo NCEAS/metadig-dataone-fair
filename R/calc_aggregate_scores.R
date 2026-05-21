988ce01417c22e862c5c73f9a5f71e1add58776a
Jeanette Clark <jclark@nceas.ucsb.edu>
2026-05-21 20:18:49 +0000

    calcaulate agg scores by hand (instead of relying on scorer)
    probably not needed

---
+++ R/calc_aggregate_scores.R
@@ -0,0 +1,68 @@
+# calculate aggregate scores
+#load packages
+library(tidyverse)
+library(here)
+library(arrow)
+
+#checks_dir <- here("../metadig-data/check-data-0.5.0")
+#runs_dir <- here("../metadig-data/run-data-0.5.0")
+
+calc_aggregate_scores <- function(checks_dir, runs_dir){
+
+    
+    checks <- open_dataset(out_checks)
+    runs <- open_dataset(out_runs)
+    
+    runs_clean <- runs %>% 
+        filter(!(run_status %in% c("ERROR", "failure", "processing")))
+    
+    checks_calc <- checks %>% 
+        mutate(check_status = if_else(grepl("error", tolower(check_output)), "ERROR", check_status)) %>% 
+        filter(check_status != "ERROR") %>% 
+        mutate(
+            # prep for formula
+            Rp = if_else(check_level == "REQUIRED" & check_status == "SUCCESS", 1, 0),
+            Rf = if_else(check_level == "REQUIRED" & check_status == "FAILURE", 1, 0),
+            Op = if_else(check_level == "OPTIONAL" & check_status == "SUCCESS", 1, 0)
+        ) %>%
+        inner_join(runs_clean) %>% 
+        group_by(obj_id, date_uploaded, format_id, origin_mn, obsoletes, obsoleted_by, check_type) %>%
+        # FAIR score by category
+        summarise(
+            type_Rp = sum(Rp, na.rm = TRUE),
+            type_Rf = sum(Rf, na.rm = TRUE),
+            type_Op = sum(Op, na.rm = TRUE),
+            # apply formula: (Rp + Op) / (Rp + Rf + Op)
+            fair_score_by_type = (type_Rp + type_Op) / (type_Rp + type_Rf + type_Op),
+        ) %>%
+        ungroup() %>% 
+        # overall FAIR score
+        group_by(obj_id, date_uploaded, format_id, obsoletes, obsoleted_by, origin_mn) %>% 
+        mutate(
+            # sum the type-level counts across all types for this obj_id
+            total_Rp = sum(type_Rp, na.rm = TRUE),
+            total_Rf = sum(type_Rf, na.rm = TRUE),
+            total_Op = sum(type_Op, na.rm = TRUE),
+            # calculate the overall score
+            fair_score = (total_Rp + total_Op) / (total_Rp + total_Rf + total_Op)
+        ) %>%
+        select(-total_Rp, -total_Rf, -total_Op, -type_Op, -type_Rp, -type_Rf) %>% 
+        collect()
+    
+    checks_agg <- checks_calc %>% 
+        tidyr::pivot_wider(names_from = check_type, values_from = fair_score_by_type) %>% 
+        rename(pid = obj_id,
+               dateUploaded = date_uploaded,
+               formatId = format_id,
+               datasource = origin_mn,
+               scoreFindable = Findable,
+               scoreAccessible = Accessible,
+               scoreInteroperable = Interoperable,
+               scoreReusable = Reusable,
+               obsoletedBy = obsoleted_by
+        )
+    
+    return(checks_agg)
+}
+
+    
