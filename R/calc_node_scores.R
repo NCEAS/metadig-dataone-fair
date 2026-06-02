1ec6fb37d3ac4c38458aec368aa8e6fd95807322
Jeanette Clark <jclark@nceas.ucsb.edu>
2026-06-02 20:03:49 +0000

    add more exploratory plots

--- R/calc_node_scores.R
+++ R/calc_node_scores.R
@@ -68,26 +68,25 @@ get_node_scores_all_time <- function(data){
 #' @importFrom dplyr mutate filter arrange group_by summarise ungroup select %>%
 #' @importFrom tidyr pivot_longer
 #' @export
-get_node_scores_mean_ym <- function(data){
+get_scores_mean_ym <- function(data){
     
     cum_repo <- data %>% 
         mutate(ym = as.Date(sprintf("%4s-%02d-01",
                                     lubridate::year(dateUploaded), 
                                     lubridate::month(dateUploaded)))) %>% 
         filter(dateUploaded > as.Date("2000-01-01")) %>% 
-        arrange(datasource, ym) %>%
-        group_by(datasource, ym) %>%
-        summarise(f=mean(scoreFindable),
+        arrange(ym) %>%
+        group_by(ym) %>%
+        summarise(num_mean = n(),
+                  f=mean(scoreFindable),
                   a=mean(scoreAccessible),
                   i=mean(scoreInteroperable),
                   r=mean(scoreReusable), .groups = "keep") %>%
         mutate(fc=cummean(f), ac=cummean(a), ic=cummean(i), rc=cummean(r)) %>%
         ungroup() %>% 
-        select(datasource, ym, f, a, i, r, fc, ac, ic, rc) %>% 
+        select(ym, f, a, i, r, fc, ac, ic, rc, num_mean) %>% 
         pivot_longer(cols = c(f, a, i, r, fc, ac, ic, rc), names_to = "metric", values_to = "score") %>% 
-        mutate(score_100 = score*100) %>% 
-        mutate(repo = gsub("urn:node:","" ,datasource)) %>% 
-        filter(!grepl("test", tolower(repo))) 
+        mutate(score_100 = score*100)
     
     return(cum_repo)
     
@@ -126,4 +125,58 @@ get_scores_rolling_mean_ym <- function(data){
     
     
     return(score_cumulative_alone)
+}
+
+get_scores_monthly_state <- function(data){
+    dt <- as.data.table(data)
+    
+    dt[, ym := as.Date(format(dateUploaded, "%Y-%m-01"))]
+    dt <- dt[dateUploaded > as.Date("2000-01-01")]
+    
+    # slice_max equivalent, get last dataset uploaded in each month
+    setorder(dt, sequenceId, ym, dateUploaded)
+    prepared <- dt[, .(
+        scoreFindable = scoreFindable[.N],
+        scoreAccessible = scoreAccessible[.N],
+        scoreInteroperable = scoreInteroperable[.N],
+        scoreReusable = scoreReusable[.N]
+    ), by = .(sequenceId, ym)]
+    
+    global_max_ym <- max(prepared$ym, na.rm = TRUE)
+    # generate full timeline grid
+    grid <- prepared[, .(ym = seq.Date(min(ym, na.rm = TRUE), global_max_ym, by = "month")), by = sequenceId]
+    
+    # set keys for matching
+    setkey(prepared, sequenceId, ym)
+    setkey(grid, sequenceId, ym)
+    
+    # 'roll = TRUE' means: if a month exists in the grid but not in prepared, 
+    # roll the values from the previous available month forward.
+    filled_dt <- prepared[grid, roll = TRUE]
+    
+    # calc monthly means
+    summary_dt <- filled_dt[, .(
+        num_mean = .N,
+        f = mean(scoreFindable, na.rm = TRUE),
+        a = mean(scoreAccessible, na.rm = TRUE),
+        i = mean(scoreInteroperable, na.rm = TRUE),
+        r = mean(scoreReusable, na.rm = TRUE)
+    ), by = .(ym)]
+    
+    # reshape
+    long_dt <- melt(
+        summary_dt, 
+        id.vars = c("ym", "num_mean"), 
+        measure.vars = c("f", "a", "i", "r"),
+        variable.name = "metric", 
+        value.name = "mean"
+    )
+    
+    long_dt[, mean := mean * 100]
+    
+    # reorder columns
+    setcolorder(long_dt, c("ym", "num_mean", "metric", "mean"))
+    setorder(long_dt, ym, metric)
+    
+    return(as.data.frame(long_dt))
 }
